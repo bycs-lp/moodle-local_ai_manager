215091b689ece0e3f847a798251e7a0a999f7c56
Philipp Memmel <philipp.memmel@mailbox.org>
2026-01-16 14:17:52 +0100

    MBS-10478: Move agent prompt into an admin setting

--- purposes/agent/version.php
+++ purposes/agent/version.php
@@ -24,7 +24,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2025121301;
+$plugin->version  = 2026011600;
 $plugin->requires = 2023042403;
 $plugin->release = '0.0.1';
 $plugin->component = 'aipurpose_agent';
