3270ad4d72fbebaa9106ca77729e52be96d82477
Hannes Funk <johannesfunk@outlook.com>
2026-03-22 12:16:58 +0100

    MBS-10395: Unit test improvements

--- tests/plugininfo/aitool_test.php
+++ tests/plugininfo/aitool_test.php
@@ -14,9 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-namespace local_ai_manager;
+namespace local_ai_manager\plugininfo;
 
-use local_ai_manager\plugininfo\aitool;
 use advanced_testcase;
 
 /**
@@ -27,9 +26,9 @@ use advanced_testcase;
  * @author     Johannes Funk
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-final class pluginfo_aitool_test extends advanced_testcase {
+final class aitool_test extends advanced_testcase {
     /**
-     * Tests the method unistall
+     * Tests the method uninstall
      *
      * @covers \local_ai_manager\plugininfo\aitool::uninstall
      */
@@ -43,24 +42,36 @@ final class pluginfo_aitool_test extends advanced_testcase {
         $instance = [
             'connector' => $connectorname,
         ];
+
         $id1 = $DB->insert_record('local_ai_manager_instance', (object)$instance);
         $id2 = $DB->insert_record('local_ai_manager_instance', (object)$instance);
+        // Fake id not equal to either id1 or id2.
+        $otherid = $id1 + $id2;
 
         // Generate three example configs: one to be deleted, two to be kept.
-        $DB->insert_record('local_ai_manager_config', (object)[
+        $config1delete = [
             'configkey' => 'purpose_somepurpose_tool_role_somerole',
             'configvalue' => $id1,
-        ]);
-
-        $DB->insert_record('local_ai_manager_config', (object)[
+        ];
+        $config2keep = [
             'configkey' => 'another_configkey',
             'configvalue' => $id2,
-        ]);
-        $otherid = $id1 + $id2;
-        $DB->insert_record('local_ai_manager_config', (object)[
+        ];
+        $config3keep = [
             'configkey' => 'purpose_somepurpose_tool_role_somerole',
             'configvalue' => $otherid,
-        ]);
+        ];
+
+        $DB->insert_record('local_ai_manager_config', $config1delete);
+        $DB->insert_record('local_ai_manager_config', $config2keep);
+        $DB->insert_record('local_ai_manager_config', $config3keep);
+
+        // Assert all records exist before uninstall.
+        $this->assertTrue($DB->record_exists('local_ai_manager_instance', ['id' => $id1, 'connector' => $connectorname]));
+        $this->assertTrue($DB->record_exists('local_ai_manager_instance', ['id' => $id2, 'connector' => $connectorname]));
+        $this->assertTrue($DB->record_exists('local_ai_manager_config', $config1delete));
+        $this->assertTrue($DB->record_exists('local_ai_manager_config', $config2keep));
+        $this->assertTrue($DB->record_exists('local_ai_manager_config', $config3keep));
 
         $pluginfo = new aitool();
         $pluginfo->name = $connectorname;
@@ -69,22 +80,11 @@ final class pluginfo_aitool_test extends advanced_testcase {
         $this->assertTrue($result);
 
         // No more instances of this connector.
-        $instancecount = $DB->count_records('local_ai_manager_instance', ['connector' => $connectorname]);
+        $instancecount = $DB->count_records('local_ai_manager_instance', $instance);
         $this->assertEquals(0, $instancecount);
 
-        $this->assertEquals(0, $DB->count_records('local_ai_manager_config', [
-            'configkey'   => 'purpose_somepurpose_tool_role_somerole',
-            'configvalue' => (string)$id1,
-        ]));
-
-        $this->assertEquals(1, $DB->count_records('local_ai_manager_config', [
-            'configkey' => 'another_configkey',
-            'configvalue' => (string)$id2,
-        ]));
-
-        $this->assertEquals(1, $DB->count_records('local_ai_manager_config', [
-            'configkey'   => 'purpose_somepurpose_tool_role_somerole',
-            'configvalue' => (string)$otherid,
-        ]));
+        $this->assertEquals(0, $DB->count_records('local_ai_manager_config', $config1delete));
+        $this->assertEquals(1, $DB->count_records('local_ai_manager_config', $config2keep));
+        $this->assertEquals(1, $DB->count_records('local_ai_manager_config', $config3keep));
     }
 }
