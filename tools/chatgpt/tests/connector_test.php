75486a7009c03cf805c554f3b6f62c889240627a
Luca Bösch <luca.boesch@bfh.ch>
2026-09-21 13:36:27 +0200

    Adhere to Moodle code checker. (#111)

--- tools/chatgpt/tests/connector_test.php
+++ tools/chatgpt/tests/connector_test.php
@@ -31,6 +31,9 @@ use local_ai_manager\local\connector_factory;
 final class connector_test extends \advanced_testcase {
     /**
      * Helper to invoke the protected get_endpoint_url() method.
+     *
+     * @param connector $connector The connector object to return the endpoint URL for
+     * @return string The endpoint URL
      */
     private function call_get_endpoint_url(connector $connector): string {
         return (new \ReflectionMethod($connector, 'get_endpoint_url'))->invoke($connector);
@@ -38,6 +41,9 @@ final class connector_test extends \advanced_testcase {
 
     /**
      * Creates a connector with a mocked instance returning the given endpoint value.
+     *
+     * @param string $endpoint the endpoint of this instance
+     * @return connector
      */
     private function make_connector(string $endpoint): connector {
         $instance = $this->getMockBuilder(\local_ai_manager\base_instance::class)
